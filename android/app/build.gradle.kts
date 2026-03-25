b99c3983ce3d2c3c1c06b4594ec1b0fb49b936ac
Vihanga-manodhya <vihangamanodhya1@gmail.com>
2026-03-25 14:38:34 +0530

    final

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,24 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 
 android {
-    namespace = "com.example.buildcare"
+    // Add this lint block to ignore the locked file check
+    lint {
+        checkReleaseBuilds = false
+    }
+
+    namespace = "com.buildcare.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +39,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.buildcare"
+        applicationId = "com.buildcare.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -33,11 +48,19 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+  signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
+   buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            // (If you had any other lines here before, like isMinifyEnabled, keep them here)
         }
     }
 }
--- android/app/src/main/kotlin/com/buildcare/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/buildcare/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.buildcare
+package com.buildcare.app
 
 import io.flutter.embedding.android.FlutterActivity
 
